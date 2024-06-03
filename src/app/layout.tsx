1293abacdabf513ac88ccc3cbc395500f31706b6
Fabio Oliveira <fabio@germinareagro.com.br>
2024-06-03 15:57:59 -0300

    add confirm popup for data visualization before finalizing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ToastContainerWrapper from "@/components/Wrapper/ToastContainerWrapper";
-import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
-
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Selector from "@/components/Selector";
 import Success from "@/components/Success";
 import { City } from "@/types/Cities";
 import { BASE_URL } from "@/utils/config";
-import { StateTransformName } from "@/utils/stateTransform";
+import { StateTransformAcronym, StateTransformName } from "@/utils/stateTransform";
 import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -29,15 +29,17 @@ export default function Home() {
   const [supplier, setSupplier] = useState<string | null>(null);
   const models: string[] = ['oferta', 'demanda'];
   const unity: string[] = ['ton', 'sc'];
-
   const [open, setOpen] = useState(false);
 
-  const handleClickOpen = () => setOpen(true);
+  const handleClickOpen = () => {
+    if (!user || !volume || !selectedProduct || !price || !selectedCity?.name) {
+      toast.error('Preencha todos os campos!');
+      return
+    }
+    setOpen(true);
+  }
+
   const handleClose = () => setOpen(false);
-  const handleConfirm = () => {
-    setOpen(false);
-    // Your confirm action here
-  };
 
   const handleProductSelect = (product: string) => {
     setSelectedProduct(product);
@@ -56,6 +58,7 @@ export default function Home() {
   }, []);
 
   const handleSubmit = async () => {
+    setOpen(false);
     const payload = {
       user: user,
       volume: volume,
@@ -98,13 +101,8 @@ export default function Home() {
     }
   };
 
-  if (showSuccess) {
-    return <Success />;
-  };
-
-  if (loading) {
-    return <Loading />;
-  };
+  if (showSuccess) return <Success />;
+  if (loading) return <Loading />;
 
   return (
     <>
@@ -205,36 +203,66 @@ export default function Home() {
               title={`Criar ${selectedModel}`}
               containerStyle="mt-10 w-[80%]"
               onClick={() => {
-                handleSubmit();
+                handleClickOpen();
               }}
             />
             <div>
-              <Button onClick={handleClickOpen}>
-                Open Confirm Dialog
-              </Button>
               <Dialog
                 open={open}
                 onClose={handleClose}
                 aria-labelledby="alert-dialog-title"
                 aria-describedby="alert-dialog-description"
               >
-                <DialogTitle id="alert-dialog-title">{"Use Google's location service?"}</DialogTitle>
+                <DialogTitle id="alert-dialog-title" className="text-neutral-300">{`Deseja criar a ${selectedModel}?`}</DialogTitle>
                 <DialogContent>
-                  <DialogContentText id="alert-dialog-description">
-                    Are you sure you want to proceed?
+                  <DialogContentText
+                    id="alert-dialog-description"
+                  >
+                    <div className="text-neutral-300 flex flex-col gap-1">
+                      <div className="flex justify-between">
+                        <p className="font-bold text-[14px]">Nome:</p><p>{user}</p>
+                      </div>
+                      <div className="flex justify-between">
+                        <p className="font-bold text-[14px]">Produto:</p><p className="ml-2">{selectedProduct}</p>
+                      </div>
+                      <div className="flex justify-between">
+                        <p className="font-bold text-[14px]">Volume:</p><p>{volume}/{selectedUnity}</p>
+                      </div>
+                      <div className="flex justify-between">
+                        <p className="font-bold text-[14px]">Preço:</p><p>{price}/{selectedUnity}</p>
+                      </div>
+                      <div className="flex justify-between">
+                        <p className="font-bold text-[14px]">Cidade:</p><p>{selectedCity?.name}/{selectedCity ? StateTransformAcronym(selectedCity?.state_id) : ""}</p>
+                      </div>
+                      {
+                        selectedModel === "oferta" ?
+                          <div className="flex justify-between">
+                            <p className="font-bold text-[14px]">Fornecedor:</p><p className="ml-2">{supplier}</p>
+                          </div>
+                          :
+                          <></>
+                      }
+                    </div>
                   </DialogContentText>
                 </DialogContent>
                 <DialogActions>
-                  <Button onClick={handleClose}>Cancel</Button>
-                  <Button onClick={handleConfirm} autoFocus>
-                    Confirm
+                  <Button
+                    containerStyle='h-[35px] border-support-error text-support-error hover:text-support-error hover:border-support-error'
+                    onClick={handleClose}>
+                    Não
+                  </Button>
+                  <Button
+                    containerStyle='h-[35px] border-support-info text-support-info hover:text-support-info hover:border-support-info'
+                    onClick={handleSubmit}
+                    autoFocus>
+                    Sim
                   </Button>
                 </DialogActions>
               </Dialog>
             </div>
           </div>
-        </main>
-      </div>
+        </main >
+      </div >
       <Footer />
     </>
   );
